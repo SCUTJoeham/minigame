a5469d22d21616f08caa501fe17a63d701d09a69
XiaoZhuBanNa <1114285985@qq.com>
2020-06-16 22:52:15 +0800

    UI
    对话框、dna点升级面板、dna按钮

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject dnaMenu;
+
+    private void Start()
+    {
+        dnaMenu.SetActive(false);
+    }
+
+    public void displayDnaMenu()
+    {
+        dnaMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void clearDnaMenu()
+    {
+        dnaMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
