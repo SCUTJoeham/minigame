df70a7719e08493b825bd50e78d425eab102deb6
XiaoZhuBanNa <1114285985@qq.com>
2020-06-17 14:29:49 +0800

    UI

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,22 +4,69 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    // 用作问号面板的打开和关闭
+    private int count = 0;
+
+    // dna panel
     public GameObject dnaMenu;
+
+    // pause dialog
+    public GameObject pauseDialog;
+
+    // prompt panel
+    public GameObject promptDialog;
 
     private void Start()
     {
         dnaMenu.SetActive(false);
+        pauseDialog.SetActive(false);
+        promptDialog.SetActive(false);
     }
 
-    public void displayDnaMenu()
+    public void DisplayDnaMenu()
     {
+        pauseDialog.SetActive(false);
+        promptDialog.SetActive(false);
         dnaMenu.SetActive(true);
         Time.timeScale = 0f;
     }
 
-    public void clearDnaMenu()
+    public void ClearDnaMenu()
     {
+        pauseDialog.SetActive(false);
+        promptDialog.SetActive(false);
         dnaMenu.SetActive(false);
         Time.timeScale = 1f;
+    }
+
+    public void DisplayPauseMenu()
+    {
+        pauseDialog.SetActive(true);
+        promptDialog.SetActive(false);
+        dnaMenu.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void ClearPauseMenu()
+    {
+        pauseDialog.SetActive(false);
+        promptDialog.SetActive(false);
+        dnaMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void PromptPanel()
+    {
+        if (count % 2 == 0)
+        {
+            dnaMenu.SetActive(false);
+            pauseDialog.SetActive(false);
+            promptDialog.SetActive(true);
+        }
+        else
+        {
+            promptDialog.SetActive(false);
+        }
+        count++;
     }
 }
